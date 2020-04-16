5115f784d6a851f86961df68b5bca05bc022c25a
Dinesh Nanda <dannynanda76@gmail.com>
2020-04-15 20:46:29 -0400

    Insertion sort algorithm

---
+++ InsertionSort/src/main/java/com/insertionSort/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.insertionSort;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+public class InsertionSort {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {5,8,1,3,9,6};
+        
+        int key, temp;
+        
+        for(int i = 1; i < arr.length; i++){
+            
+            key = arr[i];
+            
+            int j = i-1;
+            
+            while(j >= 0 && arr[j] > key){
+                
+                //swap
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                
+                j--;
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
